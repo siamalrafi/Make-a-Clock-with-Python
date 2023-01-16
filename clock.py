ad8d08775cdefe6991b678ee58ef85e72c48ca91
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-17 00:38:42 +0600

    This is the python project

--- clock.py
+++ clock.py
@@ -1,1 +1,18 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root = Tk()
+root.title('Clock')
+
+def time():
+    string = strftime('%H:%M:%S %p')
+    label.config(text=string)
+
+
+label = Label(root, font=(), background="black", foreground="cyan")
+label.pack(anchor='center')
+time()
+
+mainloop()
 print('this is clock')
